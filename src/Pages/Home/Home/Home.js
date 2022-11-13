eedd7e9b244467a1cac9bfe45ea80f22808628cc
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-11-14 02:41:38 +0600

    Testimonials also added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from '../HeroSection/HeroSection';
+import Testimonials from '../Testimonials/Testimonials';
 
 const Home = () => {
     return (
         <div>
            <HeroSection/>
+           <Testimonials></Testimonials>
         </div>
     );
 };
