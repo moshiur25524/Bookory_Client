0f7b30fb3a5c4821230ea67a17ab478cc554003a
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-11-16 03:46:09 +0600

    Deleted a book is successfull

--- src/Pages/Home/Books/ManageBooks/ManageBooks.js
+++ src/Pages/Home/Books/ManageBooks/ManageBooks.js
@@ -3,7 +3,29 @@ import useBooks from '../../../../Hooks/useBooks';
 
 const ManageBooks = () => {
     // const {_id ,book_name,image,short_description,price,quantity,supplier_name} = book;
-    const [books] = useBooks()
+    const [books, setBooks] = useBooks()
+
+    const handleDeleteBook = id => {
+        console.log(`deleting Id: ${id}`);
+
+        const proceed = window.confirm('Are You sure to Delete ?')
+        if (proceed) {
+            const url = `http://localhost:8080/book/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted Successfully');
+                        const remaining = books.filter(book => book._id !== id)
+                        setBooks(remaining)
+                    }
+
+                })
+        }
+
+    }
     return (
         <div>
             <h1>Manage all the books</h1>
@@ -35,7 +57,7 @@ const ManageBooks = () => {
                                     </thead>
                                     <tbody class="text-sm divide-y divide-gray-100">
                                         {
-                                            books.map(book =><tr>
+                                            books.map(book => <tr>
                                                 <td class="p-2 whitespace-nowrap">
                                                     <div class="flex items-center">
                                                         <div class="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img class="rounded-full" src={book.image} width="40" height="40" alt="Alex Shatov" /></div>
@@ -49,11 +71,13 @@ const ManageBooks = () => {
                                                     <div class="text-left font-medium text-green-500">{book.price}</div>
                                                 </td>
                                                 <td class="p-2 whitespace-nowrap">
-                                                    <div class="text-lg text-center">??</div>
+                                                    <div class="text-lg text-center">
+                                                        <button type="button" onClick={() => handleDeleteBook(book._id)} class="inline-block px-6 py-2.5 bg-transparent text-red-600 font-medium text-xs leading-tight uppercase rounded focus:outline-none  active:text-blue-800 transition duration-300 ease-in-out">Delete</button>
+                                                    </div>
                                                 </td>
                                             </tr>)
                                         }
-                                        
+
                                     </tbody>
                                 </table>
                             </div>
